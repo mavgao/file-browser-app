466c84e99542d4fff6a562c6038ae5974e9bcc12
mavgao <mavnotfound@gmail.com>
2024-01-01 03:30:57 -0800

    localStorage setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const persistedState = loadState();
+
 const store = configureStore({
   reducer: rootReducer,
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+  try {
+    const serializedState = JSON.stringify(store.getState());
+    localStorage.setItem("state", serializedState);
+  } catch {
+    // 忽略写入错误
+  }
 });
 
 export default store;
